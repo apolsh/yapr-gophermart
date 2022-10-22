8fa5910165783a409cea08ef47860e3d7e4f8e56
Andrey Polshkov <a.a.polshkov@gmail.com>
2022-10-22 23:28:16 +0500

    Sprint5-6: test

--- cmd/internal/gophermart/app/app.go
+++ cmd/internal/gophermart/app/app.go
@@ -1,14 +1,7 @@
 package app
 
 import (
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
 	"github.com/apolsh/yapr-gophermart/cmd/internal/gophermart/controller/http"
-	"github.com/apolsh/yapr-gophermart/cmd/internal/gophermart/service"
-	"github.com/apolsh/yapr-gophermart/cmd/internal/gophermart/storage"
 	"github.com/apolsh/yapr-gophermart/config"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
@@ -18,8 +11,8 @@ import (
 func Run(cfg *config.Config) {
 	log.Info().Msg("Starting application ...")
 
-	var userStorage storage.UserStorage = nil
-	var orderStorage storage.OrderStorage = nil
+	//var userStorage storage.UserStorage = nil
+	//var orderStorage storage.OrderStorage = nil
 	decimal.MarshalJSONWithoutQuotes = true
 	//
 	//if cfg.DatabaseType == "postgresql" {
@@ -41,31 +34,31 @@ func Run(cfg *config.Config) {
 	//	userStorage = postgres.NewUserStoragePG(pool)
 	//}
 
-	gophermartService, err := service.NewGophermartServiceImpl(*cfg, userStorage, orderStorage)
-	if err != nil {
-		log.Error().Err(err).Msg(err.Error())
-		os.Exit(1)
-	}
+	//gophermartService, err := service.NewGophermartServiceImpl(*cfg, userStorage, orderStorage)
+	//if err != nil {
+	//	log.Error().Err(err).Msg(err.Error())
+	//	os.Exit(1)
+	//}
 
 	r := chi.NewRouter()
-	http.RegisterRoutes(r, gophermartService)
+	http.RegisterRoutes(r, nil)
 
-	httpServer := http.NewServer(r, cfg)
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-
-	select {
-	case s := <-interrupt:
-		log.Info().Str("message", "process interrupted by signal: "+s.String())
-	case err := <-httpServer.GetStopSignalChannel():
-		err = fmt.Errorf("process interrupted by httpServer: %w", err)
-		log.Error().Err(err).Msg(err.Error())
-	}
-
-	err = httpServer.Shutdown()
-	if err != nil {
-		log.Err(fmt.Errorf("error during server shutdown: %w", err))
-	}
+	_ = http.NewServer(r, cfg)
+	//
+	//interrupt := make(chan os.Signal, 1)
+	//signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	//
+	//select {
+	//case s := <-interrupt:
+	//	log.Info().Str("message", "process interrupted by signal: "+s.String())
+	//case err := <-httpServer.GetStopSignalChannel():
+	//	err = fmt.Errorf("process interrupted by httpServer: %w", err)
+	//	log.Error().Err(err).Msg(err.Error())
+	//}
+	//
+	//err = httpServer.Shutdown()
+	//if err != nil {
+	//	log.Err(fmt.Errorf("error during server shutdown: %w", err))
+	//}
 
 }
