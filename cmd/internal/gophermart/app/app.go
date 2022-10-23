c3676b8ca91fa9afabe5b9e27f5bdbacb1e00557
Andrey Polshkov <a.a.polshkov@gmail.com>
2022-10-23 12:49:14 +0500

    Sprint5-6: test

--- cmd/internal/gophermart/app/app.go
+++ cmd/internal/gophermart/app/app.go
@@ -1,17 +1,11 @@
 package app
 
 import (
-	"context"
-	"errors"
 	defaultLogger "log"
 	"net/http"
-	"os"
 
-	"github.com/apolsh/yapr-gophermart/cmd/internal/gophermart/storage/postgres"
 	"github.com/apolsh/yapr-gophermart/config"
 	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/rs/zerolog/log"
 	"github.com/shopspring/decimal"
 )
 
@@ -21,24 +15,24 @@ func Run(cfg *config.Config) {
 	//var orderStorage storage.OrderStorage = nil
 	decimal.MarshalJSONWithoutQuotes = true
 
-	if cfg.DatabaseType == "postgresql" {
-		_, err := pgxpool.ParseConfig(cfg.DatabaseURI)
-		if err != nil {
-			log.Error().Err(err).Msg(err.Error())
-			os.Exit(1)
-		}
-
-		pool, err := pgxpool.Connect(context.Background(), cfg.DatabaseURI)
-		if err != nil {
-			err := errors.New("failed to connect to the database")
-			log.Error().Err(err).Msg(err.Error())
-			os.Exit(1)
-		}
-		postgres.RunMigration(cfg.DatabaseURI)
-		defer pool.Close()
-		//orderStorage = postgres.NewOrderStoragePG(pool)
-		//userStorage = postgres.NewUserStoragePG(pool)
-	}
+	//if cfg.DatabaseType == "postgresql" {
+	//	_, err := pgxpool.ParseConfig(cfg.DatabaseURI)
+	//	if err != nil {
+	//		log.Error().Err(err).Msg(err.Error())
+	//		os.Exit(1)
+	//	}
+	//
+	//	pool, err := pgxpool.Connect(context.Background(), cfg.DatabaseURI)
+	//	if err != nil {
+	//		err := errors.New("failed to connect to the database")
+	//		log.Error().Err(err).Msg(err.Error())
+	//		os.Exit(1)
+	//	}
+	//	postgres.RunMigration(cfg.DatabaseURI)
+	//	defer pool.Close()
+	//	orderStorage = postgres.NewOrderStoragePG(pool)
+	//	userStorage = postgres.NewUserStoragePG(pool)
+	//}
 
 	//gophermartService, err := service.NewGophermartServiceImpl(*cfg, userStorage, orderStorage)
 	//if err != nil {
